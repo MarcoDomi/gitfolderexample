74f3d38e003ec7279dfa67afcb1372a1f37deb73
MarcoDominguez <marcod082@gmail.com>
2023-01-14 18:41:17 -0600

    added two variables and printed a sum

--- start.cpp
+++ start.cpp
@@ -5,4 +5,9 @@ int main(){
 
     std::cout << "This is a new message!!!\n";
     std::cout << "I hope you are doing well\n";
+
+    int x = 5;
+    int y = 10;
+
+    std::cout << x + y << std::endl;
 }
