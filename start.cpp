2d9dcc1b77dab376cb0d16c82e6b9dd58c25d78f
MarcoDominguez <marcod082@gmail.com>
2023-01-14 18:00:18 -0600

    added some features

--- start.cpp
+++ start.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
 int main(){
-    std::cout << "hello world\n";
+    std::cout << "hello world!\n";
+
+    std::cout << "This is a new message!!!\n";
+    std::cout << "I hope you are doing well\n";
 }
